3eccf9027ed371c0a63c8fde6f1aa0b2f339937d
Mcrash01 <90929317+Mcrash01@users.noreply.github.com>
2025-03-04 11:41:09 +0000

    add tomogramm dataloader

---
+++ src/TomogramDataLoader.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+import numpy as np
+from config import CLASS_MAPPING
+from src.preprocessing import generate_mask, extract_grid_patches
+from tensorflow.keras.utils import to_categorical
+
+class TomogramDataLoader:
+    def __init__(self, dataset, resolution='2', num_cubes_axis=8, sphere_radius=2, augment=False):
+        self.dataset = dataset
+        self.resolution = resolution
+        self.num_cubes_axis = num_cubes_axis
+        self.sphere_radius = sphere_radius
+        self.augment = augment
+
+    def generate_patch(self, tomogram):
+        mask = generate_mask(tomogram, self.resolution, self.sphere_radius)
+        patches_img, patches_mask = extract_grid_patches(tomogram, mask, self.resolution, self.num_cubes_axis)
+        for img, mask in zip(patches_img, patches_mask):
+            img = img[..., np.newaxis].astype(np.float32)
+            mask = to_categorical(mask, num_classes=len(CLASS_MAPPING)).astype(np.uint8)
+            yield img, mask
+
+    def augment_patch(self, img, mask):
+        if self.augment:
+            if tf.random.uniform([]) > 0.5:
+                img = tf.image.flip_left_right(img)
+                mask = tf.image.flip_left_right(mask)
+            if tf.random.uniform([]) > 0.5:
+                img = tf.image.flip_up_down(img)
+                mask = tf.image.flip_up_down(mask)
+        return img, mask
+
+    def balance_patch(self, img, mask):
+        contains_protein = tf.reduce_any(tf.greater(mask[..., 1:], 0))  # Classe 0 est le fond
+        return contains_protein
+
+    def dataset_generator(self):
+        for tomogram in self.dataset:
+            for img, mask in self.generate_patch(tomogram):
+                yield img, mask
+
+    def get_dataset(self, batch_size=1):
+        dataset = tf.data.Dataset.from_generator(
+            self.dataset_generator,
+            output_signature=(
+                tf.TensorSpec(shape=(None, None, None, 1), dtype=tf.float32),
+                tf.TensorSpec(shape=(None, None, None, len(CLASS_MAPPING)), dtype=tf.uint8)
+            )
+        )
+        # Balance le dataset (50% avec protéines, 50% sans protéines)
+        dataset = dataset.filter(self.balance_patch)
+        dataset = dataset.concatenate(dataset.filter(lambda img, mask: tf.logical_not(self.balance_patch(img, mask))))
+        dataset = dataset.shuffle(1000)
+        dataset = dataset.map(self.augment_patch, num_parallel_calls=tf.data.AUTOTUNE)
+        dataset = dataset.batch(batch_size).prefetch(tf.data.AUTOTUNE)
+        return dataset
--- src/model.py
+++ src/model.py
@@ -1,30 +1,15 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Conv3D, MaxPooling3D, UpSampling3D, Cropping3D, Concatenate, Dropout, BatchNormalization, Activation, ZeroPadding3D
-from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.losses import CategoricalCrossentropy
+from tensorflow.keras import Model
+from tensorflow.keras.layers import (Input, Conv3D, MaxPooling3D, UpSampling3D, 
+                                     Cropping3D, Concatenate, Dropout, BatchNormalization, 
+                                     Activation, ZeroPadding3D)
 from tensorflow.keras.saving import register_keras_serializable
 from config import PARTICLE_WEIGHTS, PARTICLE_COLORS, CLASS_MAPPING
-
-
-# On suppose ici que le nombre total de classes (background + protéines) est 7.
-n_classes = 7
-
-def protein_accuracy(y_true, y_pred):
-    y_true_labels = tf.argmax(y_true, axis=-1)
-    y_pred_labels = tf.argmax(y_pred, axis=-1)
-    mask = tf.not_equal(y_true_labels, 0)
-    correct = tf.equal(y_true_labels, y_pred_labels)
-    correct_masked = tf.boolean_mask(correct, mask)
-
-    return tf.reduce_sum(tf.cast(correct_masked, tf.float32)) / tf.maximum(1.0, tf.reduce_sum(tf.cast(mask, tf.float32)))
+from tensorflow.keras.optimizers import Adam
 
 @register_keras_serializable()
 def weighted_categorical_crossentropy(weights_dict, class_mapping, num_classes):
-    """
-    Fonction de perte pondérée qui ignore le fond (classe 0).
-    """
     weight_vector = np.ones(num_classes, dtype=np.float32)
     for molecule, class_idx in class_mapping.items():
         if molecule in weights_dict:
@@ -34,85 +19,83 @@ def weighted_categorical_crossentropy(weights_dict, class_mapping, num_classes):
     def loss(y_true, y_pred):
         y_pred = tf.clip_by_value(y_pred, 1e-7, 1.0)
         loss_unweighted = -tf.reduce_sum(y_true * tf.math.log(y_pred), axis=-1)
-
-        # Exclure les voxels de classe 0 (fond)
         true_class = tf.argmax(y_true, axis=-1)
         mask = tf.not_equal(true_class, 0)
         voxel_weights = tf.gather(weight_vector, true_class)
         weighted_loss = loss_unweighted * voxel_weights
-
-        # Appliquer le masque : ne considérer que les protéines
         masked_loss = tf.boolean_mask(weighted_loss, mask)
 
-        return tf.cond(
-            tf.greater(tf.size(masked_loss), 0),  
-            lambda: tf.reduce_mean(masked_loss),  
-            lambda: tf.constant(0.0, dtype=tf.float32)
-        )
-
+        return tf.cond(tf.size(masked_loss) > 0, 
+                       lambda: tf.reduce_mean(masked_loss), 
+                       lambda: tf.constant(0.0, dtype=tf.float32))
     return loss
 
+class UNet3D(Model):
+    def __init__(self, input_shape=(23, 79, 79, 1), n_classes=7, filters=[16, 32, 64], dropout=0):
+        super(UNet3D, self).__init__()
+        self.input_shape = input_shape
+        self.n_classes = n_classes
+        self.filters = filters
+        self.dropout = dropout
+        self.model = self.build_model()
 
+    def conv_block(self, inputs, n_filters, dropout, batch_norm=True):
+        x = Conv3D(n_filters, kernel_size=3, padding='same')(inputs)
+        if batch_norm:
+            x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        x = Conv3D(n_filters, kernel_size=3, padding='same')(x)
+        if batch_norm:
+            x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        if dropout > 0:
+            x = Dropout(dropout)(x)
+        return x
 
-def conv_block(inputs, n_filters, dropout=0, batch_norm=True):
-    x = Conv3D(n_filters, kernel_size=3, padding='same')(inputs)
-    if batch_norm:
-        x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = Conv3D(n_filters, kernel_size=3, padding='same')(x)
-    if batch_norm:
-        x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    if dropout > 0:
-        x = Dropout(dropout)(x)
-    return x
+    def build_model(self):
+        inputs = Input(self.input_shape)
+        c1 = self.conv_block(inputs, self.filters[0], self.dropout)
+        p1 = MaxPooling3D(pool_size=(2, 2, 2), padding="same")(c1)
 
-def unet3d_model(input_shape, n_classes, filters=[16, 32, 64], dropout=0):
-    inputs = Input(input_shape)
-    # Encoder
-    c1 = conv_block(inputs, filters[0], dropout)
-    p1 = MaxPooling3D(pool_size=(2,2,2), padding="same")(c1)
-    
-    c2 = conv_block(p1, filters[1], dropout)
-    p2 = MaxPooling3D(pool_size=(2,2,2), padding="same")(c2)
-    
-    c3 = conv_block(p2, filters[2], dropout)
-    p3 = MaxPooling3D(pool_size=(2,2,2), padding="same")(c3)
-    
-    # Bottleneck
-    c4 = conv_block(p3, filters[2]*2, dropout)
-    # Decoder
-    u3 = UpSampling3D(size=(2,2,2))(c4)
-    # Ici, c3 a été obtenu avec padding="same". On s'attend à ce que u3 et c3 aient les mêmes dimensions.
-    u3 = Concatenate()([u3, c3])
-    c5 = conv_block(u3, filters[2], dropout)
-    
-    u2 = UpSampling3D(size=(2,2,2))(c5)
-    u2 = Concatenate()([u2, c2])
-    c6 = conv_block(u2, filters[1], dropout)
-    
-    u1 = UpSampling3D(size=(2,2,2))(c6)
-    # On remarque que la dimension spatiale de u1 est calculée par multiplication par 2,
-    # ce qui peut donner une taille légèrement différente de celle de c1.
-    # Par exemple, avec une entrée de (23,79,79), u1 pourrait avoir (24,80,80).
-    # On applique alors un ZeroPadding3D sur c1 pour l'ajuster.
-    c1_pad = ZeroPadding3D(padding=((0,1), (0,1), (0,1)))(c1)
-    u1 = Concatenate()([u1, c1_pad])
-    c7 = conv_block(u1, filters[0], dropout)
+        c2 = self.conv_block(p1, self.filters[1], self.dropout)
+        p2 = MaxPooling3D(pool_size=(2, 2, 2), padding="same")(c2)
 
+        c3 = self.conv_block(p2, self.filters[2], self.dropout)
+        p3 = MaxPooling3D(pool_size=(2, 2, 2), padding="same")(c3)
 
-    # Cropping pour ajuster la taille finale
-    output_crop = Cropping3D(cropping=((0,1), (0,1), (0,1)))(c7)
-    outputs = Conv3D(n_classes, kernel_size=1, activation='softmax')(output_crop)
-    
-    model = Model(inputs=inputs, outputs=outputs)
-    return model
+        c4 = self.conv_block(p3, self.filters[2] * 2, self.dropout)
 
-# Paramètres d'entrée adaptés à votre cas (par exemple, avec des patches de résolution 2)
-input_shape = (23, 79, 79, 1)
-n_classes = 7
-filters = [16, 32, 64]
-dropout = 0
+        u3 = UpSampling3D(size=(2, 2, 2))(c4)
+        u3 = Concatenate()([u3, c3])
+        c5 = self.conv_block(u3, self.filters[2], self.dropout)
 
+        u2 = UpSampling3D(size=(2, 2, 2))(c5)
+        u2 = Concatenate()([u2, c2])
+        c6 = self.conv_block(u2, self.filters[1], self.dropout)
 
+        u1 = UpSampling3D(size=(2, 2, 2))(c6)
+        c1_pad = ZeroPadding3D(padding=((0, 1), (0, 1), (0, 1)))(c1)
+        u1 = Concatenate()([u1, c1_pad])
+        c7 = self.conv_block(u1, self.filters[0], self.dropout)
 
+        output_crop = Cropping3D(cropping=((0, 1), (0, 1), (0, 1)))(c7)
+        outputs = Conv3D(self.n_classes, kernel_size=1, activation='softmax')(output_crop)
+
+        return Model(inputs=inputs, outputs=outputs)
+
+    def call(self, inputs):
+        return self.model(inputs)
+
+    def compile_model(self, learning_rate=0.001):
+        optimizer = Adam(learning_rate)
+        loss = weighted_categorical_crossentropy(PARTICLE_WEIGHTS, CLASS_MAPPING, self.n_classes)
+        self.model.compile(optimizer=optimizer, loss=loss, metrics=[self.protein_accuracy])
+
+    @staticmethod
+    def protein_accuracy(y_true, y_pred):
+        y_true_labels = tf.argmax(y_true, axis=-1)
+        y_pred_labels = tf.argmax(y_pred, axis=-1)
+        mask = tf.not_equal(y_true_labels, 0)
+        correct = tf.equal(y_true_labels, y_pred_labels)
+        correct_masked = tf.boolean_mask(correct, mask)
+        return tf.reduce_sum(tf.cast(correct_masked, tf.float32)) / tf.maximum(1.0, tf.reduce_sum(tf.cast(mask, tf.float32)))
